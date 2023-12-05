d055b51884ef0d82c8351f1b14bddb7be5b9323d
Balu Tirupati <balusekhar8@gmail.com>
2023-12-05 02:12:15 -0600

    Optimised Imports

--- app/src/main/java/com/example/elevateadmin/addTrainer/ShowTrainerAdapter.kt
+++ app/src/main/java/com/example/elevateadmin/addTrainer/ShowTrainerAdapter.kt
@@ -4,7 +4,6 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.example.elevateadmin.addClass.ClassDetails
 import com.example.elevateadmin.databinding.ItemRowAddtrainerBinding
 
 class ShowTrainerAdapter(private var trainerDetails:List<TrainerDetails>)
